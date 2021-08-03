9460d35be0cd286c93d8d2ba02bfaa4ec93c8b3a
AlexMedyantsev <medyantsev1978@icloud.com>
2021-08-03 05:20:52 +0300

    Добавил новый reducerUI

--- src/reducer/name-space.js
+++ src/reducer/name-space.js
@@ -1,7 +1,5 @@
 const NameSpace = {
-  NOTES: `NOTES`,
-  SEARCH: `SEARCH`,
-  INTERFACE: `INTERFACE`,
+  UI: `UI`,
 };
 
 export default NameSpace;
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,11 +1,7 @@
-import {reducer as reducerNotes} from "./notes/notes.js";
-import {reducer as reducerSearch} from "./search/search.js";
-import {reducer as reducerInterface} from "./interface/interface.js";
+import {reducer as reducerUI} from "./ui/ui.js";
 import NameSpace from "./name-space.js";
 import {combineReducers} from "redux";
 
 export default combineReducers({
-  [NameSpace.NOTES]: reducerNotes,
-  [NameSpace.SEARCH]: reducerSearch,
-  [NameSpace.INTERFACE]: reducerInterface,
+  [NameSpace.UI]: reducerUI,
 });
