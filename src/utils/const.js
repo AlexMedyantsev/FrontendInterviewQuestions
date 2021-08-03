fbca2ba9e33f38f0df651ba5f389c67273884bbb
AlexMedyantsev <medyantsev1978@icloud.com>
2021-08-03 05:19:56 +0300

    Добавил основные цвета приложения в отдельный объект константу

---
+++ src/utils/const.js
@@ -0,0 +1,57 @@
+export const colors = {
+  lightBlue: 'rgba(64, 156, 225)',
+  darkBlue: 'rgba(0, 64, 221)',
+
+  lightBrown: 'rgba(172, 142, 104)',
+  darkBrown: 'rgba(162, 132, 194)',
+
+  lightCyan: 'rgba(100, 210, 255)',
+  darkCyan: 'rgba(50, 173, 230)',
+
+  lightGreen: 'rgba(48, 209, 88)',
+  darkGreen: 'rgba(52, 199, 89)',
+
+  lightIndigo: 'rgba(94, 92, 230)',
+  darkIndigo: 'rgba(88, 86, 214)',
+
+  lightMint: 'rgba(102, 212, 207)',
+  darkMint: 'rgba(0, 199, 190)',
+
+  lightOrange: 'rgba(255, 159, 10)',
+  darkOrange: 'rgba(255, 149, 0)',
+
+  lightPink: 'rgba(255, 69, 58)',
+  darkPink: 'rgba(255, 59, 48)',
+
+  lightPurple: 'rgba(191, 90, 242)',
+  darkPurple: 'rgba(175, 82, 222)',
+
+  lightTeal: 'rgba(64, 200, 224)',
+  darkTeal: 'rgba(48, 176, 199)',
+
+  lightYellow: 'rgba(255, 214, 10)',
+  darkYellow: 'rgba(255, 204, 0)',
+
+  lightRed: 'rgba(255,69,58)',
+  darkRed: 'rgba(255,59,48)',
+
+
+  lightGray1: 'rgba(142, 142, 147)',
+  darkGray1: 'rgba(142, 142, 147)',
+
+  lightGray2: 'rgba(174, 174, 178)',
+  darkGray2: 'rgba(99, 99, 102)',
+
+  lightGray3: 'rgba(199, 199, 204)',
+  darkGray3: 'rgba(72, 72, 74)',
+
+  lightGray4: 'rgba(209, 209, 214)',
+  darkGray4: 'rgba(58, 58, 60)',
+
+  lightGray5: 'rgba(229, 229, 234)',
+  darkGray5: 'rgba(44, 44, 46)',
+
+  lightGray6: 'rgba(242, 242, 247)',
+  darkGray6: 'rgba(28, 28, 30)',
+}
+
