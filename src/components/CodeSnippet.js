2f950599f7d3d33a130797d42a565bf691621933
AlexMedyantsev <medyantsev1978@icloud.com>
2021-08-10 14:04:40 +0300

    Изменил компонент <CodeSnippet>.
    Теперь вместо children он принимает  code - код который он будет отображать и codeLanguage - строку обозн. тип языка

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -1,9 +1,13 @@
 import '../styles/index.scss';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {docco, a11yDark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function CodeSnippet({children}) {
+function CodeSnippet({code, codeLanguage }) {
   return (
     <div className="code">
-      {children}
+      <SyntaxHighlighter showLineNumbers={true} language={codeLanguage} customStyle={{padding: '0.8em', lineHeight: '27px', paddingTop: '1em'}} style={a11yDark}>
+        {code}
+      </SyntaxHighlighter>
     </div>
   )
 }
